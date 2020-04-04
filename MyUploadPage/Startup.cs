93975998052cd95b9c6b3b654aa3a964073c2f1e
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-04 19:12:45 +0430

    DbContext

--- MyUploadPage/Startup.cs
+++ MyUploadPage/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyUploadPage.Data;
 
 namespace MyUploadPage
 {
@@ -23,6 +25,9 @@ namespace MyUploadPage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>
+               (options => options.UseSqlite("Data Source=app.sqlite"));
+
             services.AddRazorPages();
         }
 
