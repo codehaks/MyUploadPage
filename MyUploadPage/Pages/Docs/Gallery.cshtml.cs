fce5cecdde5d388e674eb10cdb03ffbfa47a110c
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-05 04:18:34 +0430

    Fix

---
+++ MyUploadPage/Pages/Docs/Gallery.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyUploadPage.Data;
+using MyUploadPage.Models;
+
+namespace MyUploadPage.Pages.Docs
+{
+    public class GalleryModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public GalleryModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<Doc> Photos { get; set; }
+
+        public void OnGet()
+        {
+            Photos = _db.Docs.Where(d => d.Extention == ".jpg").ToList();
+        }
+
+        public IActionResult OnGetFile(int id)
+        {
+            var doc = _db.Docs.Find(id);
+            return File(doc.Data, doc.ContentType);
+        }
+    }
+}
