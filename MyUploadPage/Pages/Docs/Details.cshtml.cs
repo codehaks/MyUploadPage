4107fefc813ac4adf980af8c6fde35f2c2f964f1
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-05 04:09:14 +0430

    Details

---
+++ MyUploadPage/Pages/Docs/Details.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyUploadPage.Data;
+
+namespace MyUploadPage.Pages.Docs
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public DetailsModel(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult OnGet(int id)
+        {
+            var doc = _db.Docs.Find(id);
+            return File(doc.Data, doc.ContentType, doc.FileName);
+        }
+    }
+}
