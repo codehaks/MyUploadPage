851ca588ad0c0590071dbbe8fe3fb61a4d02f801
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-04 12:22:13 +0430

    Add project files.

---
+++ MyUploadPage/Pages/Upload.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyUploadPage.Pages
+{
+    public class UploadModel : PageModel
+    {
+        [BindProperty]
+        public IFormFile UploadFile { get; set; }
+
+        public IActionResult OnPost([FromServices] IWebHostEnvironment env)
+        {
+
+            var filePath = Path.Combine(env.ContentRootPath, "Files", UploadFile.FileName);
+            using (var stream = System.IO.File.Create(filePath)) 
+            {
+                UploadFile.CopyTo(stream);
+            }
+            return RedirectToPage("Index");
+        }
+    }
+}
