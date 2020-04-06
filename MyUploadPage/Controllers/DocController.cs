22570800b77071d471cba059965a824b7653eb19
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-06 18:30:13 +0430

    Image Sharp

--- MyUploadPage/Controllers/DocController.cs
+++ MyUploadPage/Controllers/DocController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyUploadPage.Data;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace MyUploadPage.Controllers
 {
@@ -21,6 +27,30 @@ namespace MyUploadPage.Controllers
         {
             var doc = _db.Docs.Find(id);
             return File(doc.Data,doc.ContentType,doc.FileName);
+        }
+
+        [Route("api/doc/image/{id}/{width}x{height}/{FileName?}")]
+        public IActionResult GetImage(int id,int width,int height)
+        {
+            var doc = _db.Docs.Find(id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+
+            Stream imageStream = new MemoryStream();
+
+            using (Image<Rgba32> image = Image.Load(doc.Data))
+            {
+                image.Mutate(x => x
+                     .Resize(width, height));
+                image.SaveAsJpeg(imageStream);
+
+            }
+
+            imageStream.Position = 0;
+
+            return File(imageStream, doc.ContentType);
         }
     }
 }
