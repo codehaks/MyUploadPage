b72a4c962dd74ce45db4b1c6f95986e8fa602b03
Hakim Ghods <ghods.hakim@hotmail.com>
2021-01-31 23:54:56 +0330

    read

--- MyUploadPage/Controllers/DocController.cs
+++ MyUploadPage/Controllers/DocController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlTypes;
 using MyUploadPage.Data;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
@@ -23,14 +26,39 @@ namespace MyUploadPage.Controllers
         }
 
         [Route("api/doc/file/{id}/{FileName}")]
-        public IActionResult GetFile(int id,string fileName)
+        public IActionResult GetFile(int id, string fileName)
         {
             var doc = _db.Docs.Find(id);
-            return File(doc.Data,doc.ContentType,doc.FileName);
+            return File(doc.Data, doc.ContentType, doc.FileName);
+        }
+
+        public IActionResult GetFileStream()
+        {
+
+            using TransactionScope transactionScope2 = new TransactionScope();
+
+            SqlConnection sqlConnection3 = new SqlConnection("Data Source=.;Initial Catalog = FileSystemDB; Integrated Security = True");
+
+            SqlCommand sqlCommand3 = sqlConnection3.CreateCommand();
+            sqlCommand3.CommandText = "Select FileData.PathName() As Path,GET_FILESTREAM_TRANSACTION_CONTEXT() As TransactionContext From PictureTable Where PkId = (Select Max(PkId) From PictureTable)";
+            sqlConnection3.Open();
+            SqlDataReader reader = sqlCommand3.ExecuteReader();
+            reader.Read();
+            string filePath = (string)reader["Path"];
+            byte[] transactionContext2 = (byte[])reader["TransactionContext"];
+            SqlFileStream sqlFileStream2 = new SqlFileStream
+                (filePath, transactionContext2, FileAccess.Read);
+            byte[] data = new byte[sqlFileStream2.Length];
+            sqlFileStream2.Read(data, 0, Convert.ToInt16(sqlFileStream2.Length));
+            Guid valueInserted = new Guid(data);
+            sqlFileStream2.Close();
+
+
+            return Ok();
         }
 
         [Route("api/doc/image/{id}/{width}x{height}/{FileName?}")]
-        public IActionResult GetImage(int id,int width,int height)
+        public IActionResult GetImage(int id, int width, int height)
         {
             var doc = _db.Docs.Find(id);
             if (doc == null)
